f05c638095459d25653fbae51e5a90618821a0c0
himihiromu <himihiromu@icloud.com>
2019-07-03 14:45:20 +0900

    kousin

---
+++ GUIAssist/FileChoice.java
@@ -0,0 +1,6 @@
+package GUIAssist;
+
+public class FileChoice {
+	
+	
+}
---
+++ jmagickOperation/imageConvertion.java
@@ -0,0 +1,19 @@
+package jmagickOperation;
+
+/**
+ * 
+ * @author 183108
+ *
+ */
+public abstract class imageConvertion {
+	
+	/**
+	 * 
+	 * @param pass		ファイルのフルパス
+	 * @param extension	拡張子の文字列
+	 * @return			完了したかどうか
+	 */
+	public abstract boolean convertion(String pass, String extension);
+	
+	
+}
