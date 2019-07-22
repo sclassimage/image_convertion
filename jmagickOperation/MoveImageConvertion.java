ab8ca63141bb874eb74ebecb9f87a98ef3956b33
himihiromu <himihiromu@icloud.com>
2019-07-22 10:04:27 +0900

    aaa

--- jmagickOperation/MoveImageConvertion.java
+++ jmagickOperation/MoveImageConvertion.java
@@ -1,55 +1,67 @@
 package jmagickOperation;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
-import magick.ImageInfo;
-import magick.MagickException;
-import magick.MagickImage;
+import javax.imageio.ImageIO;
+
 
 public class MoveImageConvertion extends ImageConvertion{
 
 	@Override
 	public boolean convertion(String pass, String extension) {
-		if(extension == "test") {
+			File image = new File(pass);
+			
+			String[] passArray = fileSplit(pass);
+			int strNum = passArray.length - 1;
+			passArray[strNum] = extension;
+			
+			String resultPass = link(passArray);
+			File resultImage = new File(resultPass);
+			
+			BufferedImage b;
+			try {
+				b = ImageIO.read(image);
+			} catch (IOException e1) {
+				return false;
+			}
+			int ww=b.getWidth();
+			int hh=b.getHeight();
+			
+			BufferedImage b2=new BufferedImage(ww,hh,BufferedImage.TYPE_INT_RGB);
+			Graphics g2=b2.getGraphics();
+			g2.drawImage(b,0,0,null);
+			g2.dispose();
+			
+			try {
+				ImageIO.write(b2, extension, resultImage);
+			} catch (IOException e) {
+				return false;
+			}
 			return true;
+
 		}
-		String[] passArray = fileSplit(pass);
-		int strNum = passArray.length - 1;
-		File image = new File(pass);
-		if(!(image.exists())) {
-			return false;
-		}
-		passArray[strNum] = extension;
-		String resultPass = link(passArray);
-		File resultImage = new File(resultPass);
-		try {
-			MagickImage mi = new MagickImage(new ImageInfo(image.getPath()));
-	        MagickImage mo = mi.scaleImage((int) mi.getXResolution(),(int) mi.getYResolution());
-	        mo.setFileName(resultImage.getName());
-	        mo.writeImage(new ImageInfo());
-		}catch(MagickException e2) {
-			return false;
+		
+		public String[] fileSplit(String pass) {
+			String splitstr = "[" + File.separator + ".]";
+			String[] splitPass = pass.split(splitstr);
+			return splitPass;
 		}
 
-		return true;
-	}
+		public String link(String[] strArray) {
+			StringBuffer buf = new StringBuffer();
+			int i = 0;
+			for(i = 0 ; i < strArray.length -2 ; i ++) {
 
-	public String[] fileSplit(String pass) {
-		String splitstr = "[" + File.separator + ".]";
-		String[] splitPass = pass.split(splitstr);
-		return splitPass;
-	}
+				buf.append(strArray[i] + File.separator);
+			}
+			buf.append(strArray[i]);
+			i++;
+			buf.append("." + strArray[i]);
+			return buf.toString();
 
-	public String link(String[] strArray) {
-		StringBuffer buf = new StringBuffer();
-		int i = 0;
-		for(i = 0 ; i < strArray.length -1 ; i ++) {
-
-			buf.append(strArray[i] + File.separator);
 		}
-		buf.append("." + strArray[i]);
-		return buf.toString();
-
-	}
 
 }
